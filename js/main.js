29cca07bac64bb2fcb9d48c4b71ec9d254307b51
Barnaga <bmmiwbush@gmail.com>
2020-06-21 13:41:50 +0500

    second commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(".slider__item").slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    prevArrow: $('.prev-arrow'),
+    nextArrow: $('.next-arrow')
+});
